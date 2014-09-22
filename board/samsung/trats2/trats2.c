532bffdd4a963e62ba8fdd3b1b9413d739ec09e4
Przemyslaw Marczak <p.marczak@samsung.com>
2014-09-22 11:43:06 +0200

    trats2: board: add support to dm pmic api
    Changes required to support dm pmic and dm regulator api:
    - move call to board_init_i2c() into exynos_init() - earlier init the i2c
    - remove redundant ldo setup - default hardware configuration is proper
    - adjust pmic/regulator calls to new pmic api
    
    Signed-off-by: Przemyslaw Marczak <p.marczak@samsung.com>

--- board/samsung/trats2/trats2.c
+++ board/samsung/trats2/trats2.c
@@ -23,6 +23,7 @@
 #include <usb.h>
 #include <usb/s3c_udc.h>
 #include <usb_mass_storage.h>
+#include <dm.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -151,7 +152,9 @@ int exynos_early_init_f(void)
 	return 0;
 }
 
+#ifdef CONFIG_DM_PMIC
 static int pmic_init_max77686(void);
+#endif
 
 int exynos_init(void)
 {
@@ -171,71 +174,16 @@ int exynos_init(void)
 	writel(0, &pwr->inform4);
 	writel(0, &pwr->inform5);
 
+#ifdef CONFIG_SYS_I2C_INIT_BOARD
+	board_init_i2c();
+#endif
+
 	return 0;
 }
 
 int exynos_power_init(void)
 {
-	int chrg;
-	struct power_battery *pb;
-	struct pmic *p_chrg, *p_muic, *p_fg, *p_bat;
-
-#ifdef CONFIG_SYS_I2C_INIT_BOARD
-	board_init_i2c();
-#endif
-	pmic_init(I2C_7);		/* I2C adapter 7 - bus name s3c24x0_7 */
 	pmic_init_max77686();
-	pmic_init_max77693(I2C_10);	/* I2C adapter 10 - bus name soft1 */
-	power_muic_init(I2C_10);	/* I2C adapter 10 - bus name soft1 */
-	power_fg_init(I2C_9);		/* I2C adapter 9 - bus name soft0 */
-	power_bat_init(0);
-
-	p_chrg = pmic_get("MAX77693_PMIC");
-	if (!p_chrg) {
-		puts("MAX77693_PMIC: Not found\n");
-		return -ENODEV;
-	}
-
-	p_muic = pmic_get("MAX77693_MUIC");
-	if (!p_muic) {
-		puts("MAX77693_MUIC: Not found\n");
-		return -ENODEV;
-	}
-
-	p_fg = pmic_get("MAX77693_FG");
-	if (!p_fg) {
-		puts("MAX17042_FG: Not found\n");
-		return -ENODEV;
-	}
-
-	if (p_chrg->chrg->chrg_bat_present(p_chrg) == 0)
-		puts("No battery detected\n");
-
-	p_bat = pmic_get("BAT_TRATS2");
-	if (!p_bat) {
-		puts("BAT_TRATS2: Not found\n");
-		return -ENODEV;
-	}
-
-	p_fg->parent =  p_bat;
-	p_chrg->parent = p_bat;
-	p_muic->parent = p_bat;
-
-	p_bat->pbat->battery_init(p_bat, p_fg, p_chrg, p_muic);
-
-	pb = p_bat->pbat;
-	chrg = p_muic->chrg->chrg_type(p_muic);
-	debug("CHARGER TYPE: %d\n", chrg);
-
-	if (!p_chrg->chrg->chrg_bat_present(p_chrg)) {
-		puts("No battery detected\n");
-		return 0;
-	}
-
-	p_fg->fg->fg_battery_check(p_fg, p_bat);
-
-	if (pb->bat->state == CHARGE && chrg == CHARGER_USB)
-		puts("CHARGE Battery !\n");
 
 	return 0;
 }
@@ -244,61 +192,21 @@ int exynos_power_init(void)
 static int s5pc210_phy_control(int on)
 {
 	int ret = 0;
-	unsigned int val;
-	struct pmic *p, *p_pmic, *p_muic;
+	struct udevice *pmic;
 
-	p_pmic = pmic_get("MAX77686_PMIC");
-	if (!p_pmic)
+	pmic = pmic_get_by_name(UCLASS_PMIC_REGULATOR, "max77686");
+	if (!pmic)
 		return -ENODEV;
-
-	if (pmic_probe(p_pmic))
-		return -1;
-
-	p_muic = pmic_get("MAX77693_MUIC");
-	if (!p_muic)
-		return -ENODEV;
-
-	if (pmic_probe(p_muic))
-		return -1;
 
 	if (on) {
-		ret = max77686_set_ldo_mode(p_pmic, 12, OPMODE_ON);
+		ret = pmic_set_ldo_mode(pmic, 12, OPMODE_ON);
 		if (ret)
-			return -1;
-
-		p = pmic_get("MAX77693_PMIC");
-		if (!p)
-			return -ENODEV;
-
-		if (pmic_probe(p))
-			return -1;
-
-		/* SAFEOUT */
-		ret = pmic_reg_read(p, MAX77693_SAFEOUT, &val);
-		if (ret)
-			return -1;
-
-		val |= MAX77693_ENSAFEOUT1;
-		ret = pmic_reg_write(p, MAX77693_SAFEOUT, val);
-		if (ret)
-			return -1;
-
-		/* PATH: USB */
-		ret = pmic_reg_write(p_muic, MAX77693_MUIC_CONTROL1,
-			MAX77693_MUIC_CTRL1_DN1DP2);
-
+			return -EIO;
 	} else {
-		ret = max77686_set_ldo_mode(p_pmic, 12, OPMODE_LPM);
+		ret = pmic_set_ldo_mode(pmic, 12, OPMODE_LPM);
 		if (ret)
-			return -1;
-
-		/* PATH: UART */
-		ret = pmic_reg_write(p_muic, MAX77693_MUIC_CONTROL1,
-			MAX77693_MUIC_CTRL1_UT1UR2);
+			return -EIO;
 	}
-
-	if (ret)
-		return -1;
 
 	return 0;
 }
@@ -319,66 +227,47 @@ int board_usb_init(int index, enum usb_init_type init)
 
 int g_dnl_board_usb_cable_connected(void)
 {
+#ifdef CONFIG_POWER
 	struct pmic *muic = pmic_get("MAX77693_MUIC");
 	if (!muic)
 		return 0;
 
 	return !!muic->chrg->chrg_type(muic);
+#else
+	return 1;
+#endif
 }
 #endif
-
+#ifdef CONFIG_DM_PMIC
 static int pmic_init_max77686(void)
 {
-	struct pmic *p = pmic_get("MAX77686_PMIC");
+	struct udevice *p;
 
-	if (pmic_probe(p))
-		return -1;
+	p = pmic_get_by_name(UCLASS_PMIC_REGULATOR, "max77686");
+	if (!p) {
+		error("Regulator driver not found");
+		return -ENODEV;
+	}
 
 	/* BUCK/LDO Output Voltage */
-	max77686_set_ldo_voltage(p, 21, 2800000);	/* LDO21 VTF_2.8V */
-	max77686_set_ldo_voltage(p, 23, 3300000);	/* LDO23 TSP_AVDD_3.3V*/
-	max77686_set_ldo_voltage(p, 24, 1800000);	/* LDO24 TSP_VDD_1.8V */
+	pmic_set_ldo_val(p, 21, 2800000);	/* LDO21 VTF_2.8V */
+	pmic_set_ldo_val(p, 23, 3300000);	/* LDO23 TSP_AVDD_3.3V*/
+	pmic_set_ldo_val(p, 24, 1800000);	/* LDO24 TSP_VDD_1.8V */
 
 	/* BUCK/LDO Output Mode */
-	max77686_set_buck_mode(p, 1, OPMODE_STANDBY);	/* BUCK1 VMIF_1.1V_AP */
-	max77686_set_buck_mode(p, 2, OPMODE_ON);	/* BUCK2 VARM_1.0V_AP */
-	max77686_set_buck_mode(p, 3, OPMODE_ON);	/* BUCK3 VINT_1.0V_AP */
-	max77686_set_buck_mode(p, 4, OPMODE_ON);	/* BUCK4 VG3D_1.0V_AP */
-	max77686_set_buck_mode(p, 5, OPMODE_ON);	/* BUCK5 VMEM_1.2V_AP */
-	max77686_set_buck_mode(p, 6, OPMODE_ON);	/* BUCK6 VCC_SUB_1.35V*/
-	max77686_set_buck_mode(p, 7, OPMODE_ON);	/* BUCK7 VCC_SUB_2.0V */
-	max77686_set_buck_mode(p, 8, OPMODE_OFF);	/* VMEM_VDDF_2.85V */
-	max77686_set_buck_mode(p, 9, OPMODE_OFF);	/* CAM_ISP_CORE_1.2V*/
-
-	max77686_set_ldo_mode(p, 1, OPMODE_LPM);	/* LDO1 VALIVE_1.0V_AP*/
-	max77686_set_ldo_mode(p, 2, OPMODE_STANDBY);	/* LDO2 VM1M2_1.2V_AP */
-	max77686_set_ldo_mode(p, 3, OPMODE_LPM);	/* LDO3 VCC_1.8V_AP */
-	max77686_set_ldo_mode(p, 4, OPMODE_LPM);	/* LDO4 VCC_2.8V_AP */
-	max77686_set_ldo_mode(p, 5, OPMODE_OFF);	/* LDO5_VCC_1.8V_IO */
-	max77686_set_ldo_mode(p, 6, OPMODE_STANDBY);	/* LDO6 VMPLL_1.0V_AP */
-	max77686_set_ldo_mode(p, 7, OPMODE_STANDBY);	/* LDO7 VPLL_1.0V_AP */
-	max77686_set_ldo_mode(p, 8, OPMODE_LPM);	/* LDO8 VMIPI_1.0V_AP */
-	max77686_set_ldo_mode(p, 9, OPMODE_OFF);	/* CAM_ISP_MIPI_1.2*/
-	max77686_set_ldo_mode(p, 10, OPMODE_LPM);	/* LDO10 VMIPI_1.8V_AP*/
-	max77686_set_ldo_mode(p, 11, OPMODE_STANDBY);	/* LDO11 VABB1_1.8V_AP*/
-	max77686_set_ldo_mode(p, 12, OPMODE_LPM);	/* LDO12 VUOTG_3.0V_AP*/
-	max77686_set_ldo_mode(p, 13, OPMODE_OFF);	/* LDO13 VC2C_1.8V_AP */
-	max77686_set_ldo_mode(p, 14, OPMODE_STANDBY);	/* VABB02_1.8V_AP */
-	max77686_set_ldo_mode(p, 15, OPMODE_STANDBY);	/* LDO15 VHSIC_1.0V_AP*/
-	max77686_set_ldo_mode(p, 16, OPMODE_STANDBY);	/* LDO16 VHSIC_1.8V_AP*/
-	max77686_set_ldo_mode(p, 17, OPMODE_OFF);	/* CAM_SENSOR_CORE_1.2*/
-	max77686_set_ldo_mode(p, 18, OPMODE_OFF);	/* CAM_ISP_SEN_IO_1.8V*/
-	max77686_set_ldo_mode(p, 19, OPMODE_OFF);	/* LDO19 VT_CAM_1.8V */
-	max77686_set_ldo_mode(p, 20, OPMODE_ON);	/* LDO20 VDDQ_PRE_1.8V*/
-	max77686_set_ldo_mode(p, 21, OPMODE_OFF);	/* LDO21 VTF_2.8V */
-	max77686_set_ldo_mode(p, 22, OPMODE_OFF);	/* LDO22 VMEM_VDD_2.8V*/
-	max77686_set_ldo_mode(p, 23, OPMODE_OFF);	/* LDO23 TSP_AVDD_3.3V*/
-	max77686_set_ldo_mode(p, 24, OPMODE_OFF);	/* LDO24 TSP_VDD_1.8V */
-	max77686_set_ldo_mode(p, 25, OPMODE_OFF);	/* LDO25 VCC_3.3V_LCD */
-	max77686_set_ldo_mode(p, 26, OPMODE_OFF);	/*LDO26 VCC_3.0V_MOTOR*/
+	pmic_set_buck_mode(p, 1, OPMODE_STANDBY);	/* BUCK1 VMIF_1.1V_AP */
+	pmic_set_buck_mode(p, 2, OPMODE_ON);	/* BUCK2 VARM_1.0V_AP */
+	pmic_set_buck_mode(p, 3, OPMODE_ON);	/* BUCK3 VINT_1.0V_AP */
+	pmic_set_buck_mode(p, 4, OPMODE_ON);	/* BUCK4 VG3D_1.0V_AP */
+	pmic_set_buck_mode(p, 5, OPMODE_ON);	/* BUCK5 VMEM_1.2V_AP */
+	pmic_set_buck_mode(p, 6, OPMODE_ON);	/* BUCK6 VCC_SUB_1.35V*/
+	pmic_set_buck_mode(p, 7, OPMODE_ON);	/* BUCK7 VCC_SUB_2.0V */
+	pmic_set_buck_mode(p, 8, OPMODE_OFF);	/* VMEM_VDDF_2.85V */
+	pmic_set_buck_mode(p, 9, OPMODE_OFF);	/* CAM_ISP_CORE_1.2V*/
 
 	return 0;
 }
+#endif
 
 /*
  * LCD
@@ -387,19 +276,27 @@ static int pmic_init_max77686(void)
 #ifdef CONFIG_LCD
 int mipi_power(void)
 {
-	struct pmic *p = pmic_get("MAX77686_PMIC");
+	struct udevice *p = pmic_get_by_name(UCLASS_PMIC_REGULATOR, "max77686");
+	if (!p) {
+		error("Regulator driver not found");
+		return -ENODEV;
+	}
 
 	/* LDO8 VMIPI_1.0V_AP */
-	max77686_set_ldo_mode(p, 8, OPMODE_ON);
+	pmic_set_ldo_mode(p, 8, OPMODE_ON);
 	/* LDO10 VMIPI_1.8V_AP */
-	max77686_set_ldo_mode(p, 10, OPMODE_ON);
+	pmic_set_ldo_mode(p, 10, OPMODE_ON);
 
 	return 0;
 }
 
 void exynos_lcd_power_on(void)
 {
-	struct pmic *p = pmic_get("MAX77686_PMIC");
+	struct udevice *p = pmic_get_by_name(UCLASS_PMIC_REGULATOR, "max77686");
+	if (!p) {
+		error("Regulator driver not found\n");
+		return;
+	}
 
 	/* LCD_2.2V_EN: GPC0[1] */
 	gpio_request(EXYNOS4X12_GPIO_C01, "lcd_2v2_en");
@@ -407,9 +304,8 @@ void exynos_lcd_power_on(void)
 	gpio_direction_output(EXYNOS4X12_GPIO_C01, 1);
 
 	/* LDO25 VCC_3.1V_LCD */
-	pmic_probe(p);
-	max77686_set_ldo_voltage(p, 25, 3100000);
-	max77686_set_ldo_mode(p, 25, OPMODE_LPM);
+	pmic_set_ldo_val(p, 25, 3100000);
+	pmic_set_ldo_mode(p, 25, OPMODE_ON);
 }
 
 void exynos_reset_lcd(void)
